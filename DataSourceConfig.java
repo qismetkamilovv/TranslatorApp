5fc7f1452755dd8457f373880472282fa21c6fbf
Ismat Kamilov <kamilov.ismet@gmail.com>
2023-07-23 14:12:56 +0300

    thigs todo

--- DataSourceConfig.java
+++ DataSourceConfig.java
@@ -2,10 +2,19 @@ public class DataSourceConfig {
 
     // add field with wtype Connection 
 
+    /*
+     * create DB connection with Singleton Pattern and returns it
+     * what is Singleton Pattern? you can read it her < https://www.baeldung.com/java-singleton >
+     * 
+     * return Connection
+     */
     public void connect (){
 
     }
 
+    /*
+     * Closes DB connection
+     */
     public void close (){
         
     }
